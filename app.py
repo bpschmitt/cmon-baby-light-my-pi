18be34c5a54122fd44fd6dd802b65bc2f49f6263
Brad Schmitt <bradleypschmitt@gmail.com>
2020-06-01 03:51:33 +0100

    added endpoints for colors

--- app.py
+++ app.py
@@ -5,17 +5,37 @@ import unicornhat as unicorn
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+unicorn.brightness(0.5)
+width,height=unicorn.get_shape()
+
+
 @app.route('/red', methods=['GET'])
-@app.route('/yellow', methods=['GET'])
-@app.route('/green', methods=['GET'])
-
 def red():
-    return "<h1>Red</h1>"
+  unicorn.clear()
+  unicorn.set_all(255,45,0)
+  unicorn.show()
+  return 'Red'
 
+@app.route('/yellow', methods=['GET'])
 def yellow():
-    return "<h1>Yellow</h1>"
+  unicorn.clear()
+  unicorn.set_all(255,253,0)
+  unicorn.show()
+  return 'Yellow'
 
+@app.route('/green', methods=['GET'])
 def green():
-    return "<h1>Green</h1>"
+  unicorn.clear()
+  unicorn.set_all(0,224,55)
+  unicorn.show()
+  return 'Green'
+
+@app.route('/off', methods=['GET'])
+def off():
+  unicorn.off()
+  return 'Off'
+
 
 app.run(host='0.0.0.0',port='5000')
